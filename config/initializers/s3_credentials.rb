67713bc16530bf57f1341c7b7899da3864ab6e58
maycry <byorickq@gmail.com>
2011-09-28 17:29:33 +0300

    1

--- config/initializers/s3_credentials.rb
+++ config/initializers/s3_credentials.rb
@@ -1,5 +1,1 @@
-if Rails.env == "production" 
-   S3_CREDENTIALS = { :access_key_id => ENV['AKIAJZAYIMTC47C4LRPA'], :secret_access_key => ENV['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'], :bucket => "pics.lasposadress.com.ua"} 
- else 
-   S3_CREDENTIALS = Rails.root.join("config/s3.yml")
-end
+S3_CREDENTIALS = Rails.root.join("config/s3.yml")

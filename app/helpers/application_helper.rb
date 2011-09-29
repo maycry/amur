6c21583e9c8ff3e98b40312fbeceaf3c14749e6c
maycry <byorickq@gmail.com>
2011-09-29 23:57:05 +0300

    Her1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,15 +4,14 @@ module ApplicationHelper
     url_parts = current_url.split("/")
     url_parts.slice!(0..2);
     parts_count = url_parts.size
-    
     url_parts[0]
   end
+  
   def current_category
     current_url = request.url
     url_parts = current_url.split("/")
     url_parts.slice!(0..2);
     parts_count = url_parts.size
-    
     url_parts[1]
   end
 end

7bbff86a15cb93eafec95300677cdb6d88f0b9d0
maycry <byorickq@gmail.com>
2011-10-10 12:04:52 +0300

    Fixed create product bug

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,9 +4,8 @@ class Product < ActiveRecord::Base
   belongs_to :type
   belongs_to :category
   has_many :productimages, :dependent => :destroy
-  has_one :order
+  has_many :orders
   
   accepts_nested_attributes_for :productimages, :allow_destroy => true
   
-  attr_accessible :about
 end

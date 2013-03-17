97c29009edf6e52e7adbc164499c854e715fd4b5
Tanskiy Yuriy <byorickq@gmail.com>
2013-03-17 17:30:15 +0200

    about

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Amur::Application.configure do
+  Amur::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
